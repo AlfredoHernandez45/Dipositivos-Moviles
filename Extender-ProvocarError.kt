6d2515f9f01c38d5bc0d0ef4069967c2d3379a2a
AlfredoHernandez45 <l18390045@chetumal.tecnm.mx>
2022-03-09 21:57:54 -0500

    Provando la sintaxis en println

---
+++ Extender-ProvocarError.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println("Happy Birthday!")
+    println("Jhansi")
+    println("You are 25!")
+}
+/**
+* Happy Birthday!
+* Jhansi
+* You are 25!
+ */
+
+//======================================
+//Error por falta de comillas
+
+fun main() {
+    println("Happy Birthday!")
+    println(Jhansi)
+    println("You are 25!")
+}
+/*
+*Unresolved reference: Jhansi
+*/
