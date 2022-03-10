01f2165252bb55a8071d22ce028e8169577b4420
AlfredoHernandez45 <l18390045@chetumal.tecnm.mx>
2022-03-09 22:24:11 -0500

    Saltos de linea

---
+++ Salto-de-Linea.kt
@@ -0,0 +1,25 @@
+/*
+1. Cambia las instrucciones println() por print().
+2. Ejecuta el programa. ¿Qué ocurre entonces?
+ */
+
+ fun main() {
+    println("no line break")
+    println("")
+    println("with line \n break")
+}
+// Happy Birthday!JhansiYou are 25!
+
+/*
+======================================================
+Corrige el texto para que cada parte del mensaje esté en una línea separada.
+ */
+
+ fun main() {
+    print("Happy Birthday!\n")
+    print("Jhansi\n")
+    print("You are 25!")
+}
+// Happy Birthday!
+// Jhansi
+// You are 25!
